f35db8b78aa2d15d8d89c18e7b50e01ad58c5a19
Ismaïl <nguyen.ismail@gmail.com>
2018-03-26 22:58:57 +0200

    Add footer modal

---
+++ src/components/FooterModal.vue
@@ -0,0 +1,29 @@
+<template>
+    <div>
+        <div class="modal-content">
+            <h4>Titre</h4>
+            
+            <div class="input-field col s6">
+                <input id="platform_input" type="text" autofocus />
+                <label for="platform_input">Label</label>
+            </div>
+            <div class="input-field col s6">
+                <input id="login_input" type="text"/>
+                <label for="login_input">Label</label>
+            </div>
+            <div class="input-field col s6">
+                <input id="password_input" type="text"/>
+                <label for="password_input">Label</label>
+            </div>
+            <div class="input-field col s6">
+                <input id="tags_input" type="text" />
+                <label for="tags_input">Label</label>
+            </div>
+        </div>
+
+        <div class="modal-footer">
+            <a href="#!" class="modal-action modal-close waves-effect waves-green btn-flat">Cancel</a>
+            <a href="#!" class="modal-action modal-close btn waves-effect waves-light">Add</a>
+        </div>
+    </div>
+</template>
