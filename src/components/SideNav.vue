e52a690bf376e99693c25c74321e75ec83ebcaa9
Ismaïl <nguyen.ismail@gmail.com>
2019-05-13 15:04:06 +0200

    Add desktop link on side nav

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -2,7 +2,6 @@
     <ul id="slide-out" class="side-nav">
         <li class="side-nav-header">
             <img src="/assets/img/logo.png" height="200">
-            <a href="">NewCrafts 2019</a>
         </li>
 
         <li class="secondary-background-color"><a href="/MyAgenda">My agenda</a></li>
@@ -16,12 +15,30 @@
         <li><a href="/QrCodeReader">QR Code Reader</a></li>
 
         <li><div class="divider"></div></li>
+        
+        <li><a href="https://ncrafts.io/" target="_blank">NCrafts.io</a></li>
+        <li><a href="https://videos.ncrafts.io/" target="_blank">Videos</a></li>
 
+        <li><div class="divider"></div></li>
+        <li><a href="https://ncrafts.io/"><i class="material-icons">desktop_windows</i> Desktop version</a></li>
+
+        <li><div class="divider"></div></li>
         <li><a class="subheader">Follow us</a></li>
-        <li><a href="http://ncrafts.io/" target="_blank">NCrafts.io</a></li>
-        <li><a href="https://twitter.com/ncraftsConf" target="_blank">Twitter</a></li>
-        <li><a href="https://github.com/ncraftsconf/" target="_blank">Github</a></li>
-        <li><a href="https://vimeo.com/newcrafts" target="_blank">Vimeo</a></li>
+        <li>
+            <table>
+                <tr>
+                    <td>
+                        <a href="https://twitter.com/ncraftsConf"><img src="/assets/img/icon_twitter.png" alt="Twitter"></a>
+                    </td>
+                    <td>
+                        <a href="https://github.com/ncraftsconf/"><img src="/assets/img/icon_github.png" alt="Github"></a>
+                    </td>
+                    <td>
+                        <a href="https://vimeo.com/newcrafts"><img src="/assets/img/icon_vimeo.png" alt="Vimeo"></a>
+                    </td>
+                </tr>
+            </table>
+        </li>
     </ul>
 </template>
 
@@ -44,5 +61,9 @@
 
     li.side-nav-header>a {
         color: #39c8b7;
+    }
+
+    .side-nav li>a>i.material-icons {
+        color: #fff;
     }
 </style>
