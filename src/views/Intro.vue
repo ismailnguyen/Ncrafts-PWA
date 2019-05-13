69932317d14293aa70177d6b3eb6f9b7e75bc22f
Ismaïl <nguyen.ismail@gmail.com>
2019-05-13 14:53:57 +0200

    Add intro page

---
+++ src/views/Intro.vue
@@ -0,0 +1,56 @@
+<template>
+    <div class="container">
+        <div  class="event col s12 m6 l4">
+        <div class="chip">
+            NCrafts 2019
+        </div>
+
+        <div class="card rounded">
+            <div class="card-reveal">
+                <span @click="closeIntro()" class="card-title grey-text text-darken-4">Welcome aboard !<i class="material-icons right">close</i></span>
+                <p>
+                    -- I don't know what to put here --
+                </p>
+                <p>
+                    -- I don't know what to put here --
+                </p>
+                <p>
+                    -- I don't know what to put here --
+                </p>
+                <p>
+                    -- I don't know what to put here --
+                </p>
+            </div>
+            <div class="card-action">
+                <a @click="closeIntro()" class="btn">Let's go !</a>
+            </div>
+        </div>
+    </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        methods: {
+            closeIntro: function () {
+                localStorage.setItem('introAlreadyShown', true);
+                location.reload();
+            }
+        }
+    }
+</script>
+
+<style scoped>
+    div.container {
+        padding-top: 50px;
+    }
+
+    .card-image {
+        background-color: #262627;
+    }
+
+    .card-reveal {
+        display: block;
+        position: relative;
+    }
+</style>
