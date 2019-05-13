523eba97dd456b8b8eeb03c293f3cc26324c1171
Ismaïl <nguyen.ismail@gmail.com>
2019-05-13 15:02:50 +0200

    Add link to home page on logo

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
-        <a v-if="title" class="brand-logo center">{{ title }}</a>
-        <a v-else class="brand-logo center"><img src="/assets/img/only_newcrafts_blanc.png" height="40"></a>
+        <a v-if="title" class="brand-logo center" href="/">{{ title }}</a>
+        <a v-else class="brand-logo center" href="/"><img src="/assets/img/only_newcrafts_blanc.png" height="40"></a>
         
         <a data-activates="slide-out" class="show-on-large button-collapse"><i class="material-icons">menu</i></a>
     </div>
