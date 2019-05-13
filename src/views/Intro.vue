2fc4c541824adf6a01680fc1038b80f00b971370
Ismaïl <nguyen.ismail@gmail.com>
2019-05-13 18:06:47 +0200

    Make intro bloc bigger

--- src/views/Intro.vue
+++ src/views/Intro.vue
@@ -1,31 +1,33 @@
 <template>
     <div class="container">
-        <div  class="event col s12 m6 l4">
-        <div class="chip">
-            NCrafts 2019
-        </div>
+        <div class="row">
+            <div class="event col s12 m6 l4">
+                <div class="chip">
+                    NCrafts 2019
+                </div>
 
-        <div class="card rounded">
-            <div class="card-reveal">
-                <span @click="closeIntro()" class="card-title grey-text text-darken-4">Welcome aboard !<i class="material-icons right">close</i></span>
-                <p>
-                    - Access and build your full agenda with <b>session</b> and <b>speaker</b> details to help you find the right sessions for you
-                </p>
-                <p>
-                    - To build your agenda you can <b>bookmark</b> your preferred sessions
-                </p>
-                <p>
-                    - Once built, you can have access to your agenda <b>offline!</b>, simply go to [<b>my agenda</b>]
-                </p>
-                <p>
-                    - Share your thoughts with the speaker and give your <b>feedback</b>
-                </p>
-            </div>
-            <div class="card-action">
-                <a @click="closeIntro()" class="btn">Let's go !</a>
+                <div class="card rounded">
+                    <div class="card-reveal">
+                        <span @click="closeIntro()" class="card-title grey-text text-darken-4">Welcome aboard !<i class="material-icons right">close</i></span>
+                        <p>
+                            - Access and build your full agenda with <b>session</b> and <b>speaker</b> details to help you find the right sessions for you
+                        </p>
+                        <p>
+                            - To build your agenda you can <b>bookmark</b> your preferred sessions
+                        </p>
+                        <p>
+                            - Once built, you can have access to your agenda <b>offline!</b>, simply go to [<b>my agenda</b>]
+                        </p>
+                        <p>
+                            - Share your thoughts with the speaker and give your <b>feedback</b>
+                        </p>
+                    </div>
+                    <div class="card-action">
+                        <a @click="closeIntro()" class="btn">Let's go !</a>
+                    </div>
+                </div>
             </div>
         </div>
-    </div>
     </div>
 </template>
 
@@ -50,7 +52,14 @@
     }
 
     .card-reveal {
+        height: 60vh;
         display: block;
         position: relative;
+        background: none;
+    }
+
+    .card-action {
+        background: none;
+        border: none;
     }
 </style>
