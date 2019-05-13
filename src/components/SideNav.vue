b3e1069a96e82527a4132995d1e87ed100f94089
Ismaïl <nguyen.ismail@gmail.com>
2019-05-13 16:04:37 +0200

    Add green background color behind active page on menu

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -4,15 +4,15 @@
             <img src="/assets/img/logo.png" height="200">
         </li>
 
-        <li class="secondary-background-color"><a href="/MyAgenda">My agenda</a></li>
+        <li><a href="/MyAgenda" :class="isCurrentPath('/MyAgenda') ? 'secondary-background-color': ''">My agenda</a></li>
         <li><div class="divider"></div></li>
 
-        <li><a href="/">Schedule</a></li>
-        <li><a href="/Speakers">Speakers</a></li>
+        <li><a href="/" :class="isCurrentPath('/') ? 'secondary-background-color': ''">Schedule</a></li>
+        <li><a href="/Speakers" :class="isCurrentPath('/Speakers') ? 'secondary-background-color': ''">Speakers</a></li>
 
         <li><div class="divider"></div></li>
 
-        <li><a href="/QrCodeReader">QR Code Reader</a></li>
+        <li><a href="/QrCodeReader" :class="isCurrentPath('/QrCodeReader') ? 'secondary-background-color': ''">QR Code Reader</a></li>
 
         <li><div class="divider"></div></li>
         
@@ -20,6 +20,7 @@
         <li><a href="https://videos.ncrafts.io/" target="_blank">Videos</a></li>
 
         <li><div class="divider"></div></li>
+        <li><a href="/Intro" :class="isCurrentPath('/Intro') ? 'secondary-background-color': ''">Help</a></li>
         <li><a href="https://ncrafts.io/"><i class="material-icons">desktop_windows</i> Desktop version</a></li>
 
         <li><div class="divider"></div></li>
@@ -41,6 +42,22 @@
         </li>
     </ul>
 </template>
+
+<script>
+    import EventMini from '../components/EventMini.vue'
+
+    export default {
+        props: ['events'],
+        components: {
+            EventMini,
+        },
+        methods: {
+            isCurrentPath: function (path) {
+                return this.$route.path === path;
+            }
+        }
+    }
+</script>
 
 <style scoped>
     .side-nav {
