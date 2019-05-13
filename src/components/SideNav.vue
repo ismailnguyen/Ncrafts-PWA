45a4e5b2f01c6b2559fc1215bb14436595d7ee72
Ismaïl <nguyen.ismail@gmail.com>
2019-05-13 18:40:41 +0200

    Update side nav design

--- src/components/SideNav.vue
+++ src/components/SideNav.vue
@@ -4,28 +4,32 @@
             <img src="/assets/img/logo.png" height="100">
         </li>
 
-        <li><a href="/MyAgenda" :class="isCurrentPath('/MyAgenda') ? 'secondary-background-color': ''">My agenda</a></li>
         <li><div class="divider"></div></li>
-
-        <li><a href="/" :class="isCurrentPath('/') ? 'secondary-background-color': ''">Schedule</a></li>
-        <li><a href="/Speakers" :class="isCurrentPath('/Speakers') ? 'secondary-background-color': ''">Speakers</a></li>
-
+        <li><a href="/MyAgenda" :class="isCurrentPath('/MyAgenda') ? 'secondary-background-color rounded': 'rounded'">My agenda</a></li>
         <li><div class="divider"></div></li>
-
-        <li><a href="/QrCodeReader" :class="isCurrentPath('/QrCodeReader') ? 'secondary-background-color': ''">QR Code Reader</a></li>
+        <li><a href="/" :class="isCurrentPath('/') ? 'secondary-background-color rounded': 'rounded'">Schedule</a></li>
 
         <li><div class="divider"></div></li>
+        <li><a href="/Speakers" :class="isCurrentPath('/Speakers') ? 'secondary-background-color rounded': 'rounded'">Speakers</a></li>
+
+        <li><div class="divider"></div></li>
+        <li><a href="/QrCodeReader" :class="isCurrentPath('/QrCodeReader') ? 'secondary-background-color rounded': 'rounded'">QR Code Reader</a></li>
+
+        <li><div class="divider"></div></li>
+        <li><a href="https://ncrafts.io/" target="_blank" class="rounded">NCrafts.io</a></li>
         
-        <li><a href="https://ncrafts.io/" target="_blank">NCrafts.io</a></li>
-        <li><a href="https://videos.ncrafts.io/" target="_blank">Videos</a></li>
+        <li><div class="divider"></div></li>
+        <li><a href="https://videos.ncrafts.io/" target="_blank" class="rounded">Videos</a></li>
 
         <li><div class="divider"></div></li>
-        <li><a href="/Intro" :class="isCurrentPath('/Intro') ? 'secondary-background-color': ''">Help</a></li>
-        <li><a href="https://ncrafts.io/"><i class="material-icons">desktop_windows</i> Desktop version</a></li>
+        <li><a href="/Intro" :class="isCurrentPath('/Intro') ? 'secondary-background-color rounded': 'rounded'">Help</a></li>
 
         <li><div class="divider"></div></li>
-        <li><a class="subheader">Follow us</a></li>
-        <li>
+        <li><a href="https://ncrafts.io/" class="rounded">Desktop version</a></li>
+
+        <li><div class="divider"></div></li>
+        <li><a class="subheader rounded-top">Follow us</a></li>
+        <li class="rounded-bottom">
             <table>
                 <tr>
                     <td>
@@ -63,31 +67,56 @@
     .side-nav {
         background-color: #26262733;
         padding-bottom: 140px;
-        box-shadow: 0 20px 50px rgba(85, 85, 85, 0.8);
+        position: fixed; /* Temp fix for Firefox */
     }
   
-  .side-nav::before {
-    content: "";
-    position: absolute;
-    z-index: -2;
-    top: 0; right: 0; bottom: 0; left: 0;
-    background: inherit;
-    filter: contrast(1) blur(5px) grayscale(1) brightness(1);
-    margin: -10px;
-    -webkit-filter: blur(10px);
-          filter: blur(10px);
-  }
+    .side-nav::before {
+        content: "";
+        position: absolute;
+        z-index: -2;
+        top: 0; right: 0; bottom: 0; left: 0;
+        background: inherit;
+        filter: contrast(1) blur(5px) grayscale(1) brightness(1);
+        margin: -10px;
+        -webkit-filter: blur(10px);
+            filter: blur(10px);
+    }
 
-  .side-nav::after {
-    content: "";
-    position: absolute;
-    z-index: -1;
-    top: 0; right: 0; bottom: 0; left: 0;
-    background-color: #262627cc;
-  }
+    .side-nav::after {
+        content: "";
+        position: absolute;
+        z-index: -1;
+        top: 0; right: 0; bottom: 0; left: 0;
+        background-color: #262627cc;
+    }
 
-    .side-nav li>a {
+    .side-nav li {
+        margin-left: 10px;
+        margin-right: 10px;
+    }
+
+    .divider {
+        background: none;
+    }
+
+    .side-nav li>a,
+    .side-nav table {
+        background-color: #262627;
         color: rgba(255, 255, 255 ,0.87);
+    }
+
+    .rounded {
+        border-radius: 15px;
+    }
+
+    .rounded-top {
+        border-top-left-radius: 15px;
+        border-top-right-radius: 15px;
+    }
+
+    .rounded-bottom {
+        border-bottom-right-radius: 15px;
+        border-bottom-left-radius: 15px;
     }
 
     .side-nav .subheader {
@@ -98,11 +127,4 @@
         margin: 40px;
     }
 
-    li.side-nav-header>a {
-        color: #39c8b7;
-    }
-
-    .side-nav li>a>i.material-icons {
-        color: #fff;
-    }
 </style>
